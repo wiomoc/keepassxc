f7da3ba69b5dc4f3643af8c2c7f46f88700201cc
angelsl <angelsl@in04.sg>
2017-11-13 02:27:32 +0800

    Add ChaCha20 support

--- src/crypto/SymmetricCipher.h
+++ src/crypto/SymmetricCipher.h
@@ -33,7 +33,9 @@ public:
     {
         Aes256,
         Twofish,
-        Salsa20
+        Salsa20,
+        ChaCha20,
+        InvalidAlgorithm = -1
     };
 
     enum Mode
@@ -41,7 +43,8 @@ public:
         Cbc,
         Ctr,
         Ecb,
-        Stream
+        Stream,
+        InvalidMode = -1
     };
 
     enum Direction
@@ -75,8 +78,10 @@ public:
     int blockSize() const;
     QString errorString() const;
 
-    static SymmetricCipher::Algorithm cipherToAlgorithm(Uuid cipher);
-    static Uuid algorithmToCipher(SymmetricCipher::Algorithm algo);
+    static Algorithm cipherToAlgorithm(Uuid cipher);
+    static Uuid algorithmToCipher(Algorithm algo);
+    static int algorithmIvSize(Algorithm algo);
+    static Mode algorithmMode(Algorithm algo);
 
 private:
     static SymmetricCipherBackend* createBackend(SymmetricCipher::Algorithm algo, SymmetricCipher::Mode mode,
