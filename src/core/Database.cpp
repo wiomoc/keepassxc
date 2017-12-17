2d0153924cb1a7061a0a1803f72b2e14918dc263
Janek Bevendorff <janek@jbev.net>
2017-12-17 18:06:02 +0100

    Fix regression where database was marked as modified after saving

--- src/core/Database.cpp
+++ src/core/Database.cpp
@@ -217,7 +217,6 @@ bool Database::setKey(const CompositeKey& key, bool updateChangedTime, bool upda
     if (updateChangedTime) {
         m_metadata->setMasterKeyChanged(QDateTime::currentDateTimeUtc());
     }
-    emit modifiedImmediate();
 
     return true;
 }
